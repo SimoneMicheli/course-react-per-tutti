ac14034232f126e095730a6a42cee968e5483f56
Simone Micheli <simone.micheli@it.abb.com>
2022-08-01 11:02:05 +0200

    Added linter config

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: ["react-app", "react-app/jest", "plugin:react/recommended", "standard", "plugin:@typescript-eslint/recommended", "prettier"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: "latest",
+		sourceType: "module"
+	},
+	plugins: ["react", "@typescript-eslint", "unused-imports"],
+	rules: {
+		"@typescript-eslint/no-unused-vars": "off",
+		"unused-imports/no-unused-imports": "warn",
+		"unused-imports/no-unused-vars": ["warn", { vars: "all", args: "after-used", argsIgnorePattern: "^_" }]
+	}
+};
