12e925287bc409e51ff6578d234fce14ec6f0d62
Simone Micheli <simone.micheli@it.abb.com>
2022-08-13 12:30:21 +0200

    refactor: rename minimum delay function and URL

--- src/api.ts
+++ src/api.ts
@@ -1,28 +1,18 @@
 import axios from "axios"
 import { ToDo } from "./models"
 
-function delay<T>(fn: Promise<T> | (() => Promise<T>), time: number) {
-  return Promise.all([new Promise((resolve) => setTimeout(resolve, time)), typeof fn === "function" ? fn() : fn]).then(
-    (rx) => rx[1]
-  )
+async function waitMinimum<T>(fn: Promise<T> | (() => Promise<T>), time: number) {
+  const rx = await Promise.all([
+    new Promise((resolve) => setTimeout(resolve, time)),
+    typeof fn === "function" ? fn() : fn,
+  ])
+  return rx[1]
 }
 
 export function getToDoList() {
-  /* return Promise.all([
-    new Promise((resolve) => setTimeout(resolve, 5000)),
+  return waitMinimum(
     axios
-      .get<Array<ToDo>>("http://localhost:5000/api/todolist")
-      .then((resp) => {
-        return resp.data
-      })
-      .catch((e) => {
-        console.error(e)
-      }),
-  ]).then((response) => response[1]) */
-
-  return delay(
-    axios
-      .get<Array<ToDo>>("http://localhost:5000/api/todolist")
+      .get<Array<ToDo>>("http://localhost:5000/api/todo/")
       .then((resp) => {
         return resp.data
       })
