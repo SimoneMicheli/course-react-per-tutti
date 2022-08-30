2e8243f6c333bd9ab1f1229d8d212a83a6a380c0
Simone Micheli <simone.micheli@it.abb.com>
2022-08-30 18:05:40 +0200

    feat: add dates

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react"
 import { ToDo } from "../models"
 import cn from "classnames"
+import ToDoElement from "./ToDoElement"
 
 interface ToDoListProps {
   items: Array<ToDo>
@@ -56,38 +57,7 @@ export default function ToDoList(props: ToDoListProps) {
   return (
     <ul className="list-group">
       {items.map((item, index) => (
-        <li
-          key={index}
-          className={cn("list-group-item d-flex justify-content-between align-items-center", {
-            completed: item.completed,
-          })}
-        >
-          <div>
-            <input
-              type="checkbox"
-              className="form-check-input me-1"
-              checked={item.completed}
-              onChange={() => {
-                onClick(index)
-              }}
-            />
-            <label className="form-check-label">{item.title}</label>
-          </div>
-          <div className="btn-group">
-            <button className="btn btn-link text-black-50">
-              <i className="edit"></i>
-            </button>
-            <button
-              className="btn btn-link text-black-50"
-              onClick={(e) => {
-                e.preventDefault()
-                onDelete(index)
-              }}
-            >
-              <i className="trash"></i>
-            </button>
-          </div>
-        </li>
+        <ToDoElement todo={item} index={index} key={index} onClick={onClick} onDelete={onDelete} />
       ))}
     </ul>
   )
